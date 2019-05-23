707f11387f3a5283b2466e4d6d5cb9ad5b4662bd
Remco32 <remco32@gmail.com>
2019-05-23 14:12:41 +0200

    Start with code

---
+++ gameComponents.py
@@ -0,0 +1,22 @@
+
+
+# The 50 cards. 5 colours, with 10 cards each
+# values of the cards 1, 1, 1, 2, 2, 3, 3, 4, 4, 5
+
+class Card:
+    def __init__(self, colour, value, ID):
+        self.colour = colour    # {red, yellow, green, blue, white}
+        self.value = value      # 1 to 5
+        #self.cardID = ID        # unique ID for card
+
+class Deck:
+
+
+    def generateDeck:
+        # generate suit
+        for suitIterator in range(5):
+            for i in range(3):
+
+
+
+#class Discard:
