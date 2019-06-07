8dcb9475ef832b19cdea5f39ea68fac529782161
clementr <clementr.ut.ia@gmail.com>
2019-06-07 15:30:27 +0200

    Start model

--- gameSettings.py
+++ gameSettings.py
@@ -3,3 +3,4 @@
 show_own_hand = True
 use_rainbow_card = False # TODO should replace one suit instead of adding this suit to the set
 player_amount = 2  # 5 cards in the hand when 2-3 players, 4 cards for 4-5 players
+suit_amount = 5 # TODO allow changing total number of suits
