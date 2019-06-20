46ba2173753b1abf0c88ec0cc67f796f70183186
Remco32 <remco32@gmail.com>
2019-06-20 11:33:16 +0200

    Fancy run_game.py

--- run_game.py
+++ run_game.py
@@ -1,4 +1,8 @@
 import table
 import gameFlow
+import model
 
-gameFlow.gameloop(table.Table())
+#gameFlow.gameloop(table.Table())
+table = table.Table()
+model = model.HanabiModel(table.deck, table.player_list)
+gameFlow.gameloop(table)
