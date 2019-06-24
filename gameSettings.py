753a5c57d6e08a7cebe907397be1845915bfa222
Remco32 <remco32@gmail.com>
2019-06-24 21:28:04 +0200

    Added separate boolean for agent knowledge prints

--- gameSettings.py
+++ gameSettings.py
@@ -2,12 +2,13 @@
 
 # Game settings
 use_rainbow_card = True     # Boolean for using the rainbow suit.
-player_amount = 2
+player_amount = 4
 suit_amount = 5             # Including rainbow. Max being 5; 6 when using rainbow.
 hand_size = 4               # Official rules: 5 cards in the hand when 2-3 players, 4 cards for 4-5 players.
 use_single_cards = False     # Only use a single instance of a card (i.e. only one red one is in the game)
 
 # Program settings
-CHEAT_show_own_hand = True        # Prints own hand when playing a card. For testing.
-DEBUG_prints_agents = True    # Print debug text about which steps the agent is checking.
 use_human_player = False
+CHEAT_show_own_hand = True        # Prints own hand when playing a card. For testing.
+DEBUG_strategy_prints_agents = False    # Print debug text about which steps the agent is checking.
+DEBUG_print_knowledge_agents = True
